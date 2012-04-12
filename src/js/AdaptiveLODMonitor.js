38dc2552c22163fcda898870e26320d7d6b38d69
AJ <42003122+code7885@users.noreply.github.com>
2012-04-12 23:35:59 +0200

    Made the listener a member so it could be removed from the VRPanorama.

--- src/js/AdaptiveLODMonitor.js
+++ src/js/AdaptiveLODMonitor.js
@@ -19,9 +19,9 @@
  * get a render listener.
  */
 bigshot.AdaptiveLODMonitor = function (parameters) {
-	this.setParameters (parameters);
-
-	this.currentAdaptiveMagnification = parameters.vrPanorama.getMaxTextureMagnification ();
+    this.setParameters (parameters);
+    
+    this.currentAdaptiveMagnification = parameters.vrPanorama.getMaxTextureMagnification ();
     
     this.frames = 0;
     this.samples = 0;
@@ -35,6 +35,11 @@ bigshot.AdaptiveLODMonitor = function (parameters) {
     this.hqRender = false;
     this.hqMode = false;
     this.hqRenderWaiting = false;
+    
+    var that = this;
+    this.listenerFunction = function (state, cause, data) {
+        that.listener (state, cause, data);
+    };         
 };
 
 bigshot.AdaptiveLODMonitor.prototype = {
@@ -45,17 +50,17 @@ bigshot.AdaptiveLODMonitor.prototype = {
             return -1;
         }
     },
-	
-	/**
+    
+    /**
 	 * @param {bigshot.AdaptiveLODMonitorParameters} parameters
 	 */
-	setParameters : function (parameters) {
-	    this.parameters = parameters;
-    	this.targetTime = 1000 / this.parameters.targetFps;
-    
-    	this.lowerTime = this.targetTime / (1.0 + this.parameters.tolerance);
-    	this.upperTime = this.targetTime * (1.0 + this.parameters.tolerance);
-	},
+    setParameters : function (parameters) {
+        this.parameters = parameters;
+        this.targetTime = 1000 / this.parameters.targetFps;
+        
+        this.lowerTime = this.targetTime / (1.0 + this.parameters.tolerance);
+        this.upperTime = this.targetTime * (1.0 + this.parameters.tolerance);
+    },
     
     averageRenderTimeLast : function () {
         if (this.samples > 0) {
@@ -64,14 +69,11 @@ bigshot.AdaptiveLODMonitor.prototype = {
             return -1;
         }
     },
-	
-	getListener : function () {
-		var that = this;
-		return function (state, cause, data) {
-            that.listener (state, cause, data);
-        }                    
-	},
-	   
+    
+    getListener : function () {
+        return this.listenerFunction;
+    },
+    
     increaseDetail : function () {
         this.currentAdaptiveMagnification = Math.max (this.parameters.minMag, this.currentAdaptiveMagnification / (1.0 + this.parameters.rate));
     },
